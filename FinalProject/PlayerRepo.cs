7a5542e1db7fa596766803f8b75edb703e52d213
David Bertus-Barcza <davi7805@edu.eal.dk>
2017-05-05 14:37:08 +0200

    Add project files.

---
+++ FinalProject/PlayerRepo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject
+{
+    class PlayerRepo
+    {
+        public List<Players> Players = new List<Players>();
+        private void buttonAddplayer_Click(object sender, EventArgs e)
+
+        {
+            Players.Add(firstNameBox.Text);
+        }
+             
+
+    }
+}
